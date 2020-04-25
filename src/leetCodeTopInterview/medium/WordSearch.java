9b2c84cdec46b829f9fe510098eafe36782c5d01
sumandroid <sumansaurabh93s@gmail.com>
2020-04-26 03:31:25 +0530

    medium level questions' solutions first commit

---
+++ src/leetCodeTopInterview/medium/WordSearch.java
@@ -0,0 +1,44 @@
+package leetCodeTopInterview.medium;
+
+public class WordSearch {
+    //problem: https://leetcode.com/problems/word-search/
+
+    public static void main(String []args){
+        char[][] board = new char[][]{
+                {'A','B','C','E'},
+                {'S','F','C','S'},
+                {'A','D','E','E'}};
+
+        String word = "ABCCED";
+        System.out.println(exists(board, word));
+    }
+
+    private static boolean exists(char[][] board, String word){
+        if(board.length == 0){
+            return false;
+        }
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                if(board[i][j] == word.charAt(0) && dfs(board, i, j, 0, word)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean dfs(char[][] board, int i, int j, int count, String word){
+        if(count == word.length()){
+            return true;
+        }
+        if(i < 0 || i >= board.length || j < 0 || j >= board[i].length || board[i][j] != word.charAt(count)){
+            return false;
+        }
+        char temp = board[i][j];
+        board[i][j] = ' ';
+        boolean found = dfs(board, i-1, j, count+1, word) || dfs(board, i+1, j, count+1, word) ||
+                dfs(board, i, j-1, count+1, word) || dfs(board, i,j+1, count+1, word);
+        board[i][j] = temp;
+        return found;
+    }
+}
